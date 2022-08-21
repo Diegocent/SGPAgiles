699eaf2c8fbee243005a2526a28b342f0e3aef31
marcelo <marce_molas@gmail.com>
2022-08-21 19:57:14 -0400

    Nuevo requerimiento del paquete de postgres y añadi al settings.py la confi del database

--- SGPAgiles/settings.py
+++ SGPAgiles/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'SGPAgiles.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'djangoproject.localhost',
+        'PORT': '5432',
     }
 }
 
