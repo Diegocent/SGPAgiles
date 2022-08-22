376072bd224612a134a721d832c6c8e523898c8e
marcelo <marce_molas@gmail.com>
2022-08-22 11:10:15 -0400

    Agregue un requerimiento para el frontend, y modifique el settings.py para que funcione el sso.

--- SGPAgiles/settings.py
+++ SGPAgiles/settings.py
@@ -128,7 +128,23 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-SITE_ID = 1
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
+
 # Provider specific settings
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+SITE_ID = 2
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
