18477ae9e96ed3470b59a4549e8f6f31492d5da2
marcelo <marce_molas@gmail.com>
2022-10-16 17:24:26 -0300

    se modifico el view para login, si hay un solo usuario y ese usuario no es admin. se le vuelve admin.

--- login/views.py
+++ login/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from Usuario.models import Usuario
+from Usuario.models import Usuario, RolSistema, Permisos
 
 
 """
@@ -11,5 +11,25 @@ Actualmente contamos con los siguientes views:
 """
 # === login ===
 def login(request):
-        return render(request, 'index.html')
+        if not request.user.is_authenticated:
+                return render(request, 'index.html')
+        else:
+                cantidad_de_usuarios = Usuario.objects.count()
+                unico_usuario_es_admin = request.user.es_admin()
+                #si hay un solo usuario y el current usuario no es admin, se le asigna como admin
+                if(cantidad_de_usuarios == 1 and not unico_usuario_es_admin):
+                        array_de_roles = RolSistema.objects.all().filter(nombre="admin")
+                        if len(array_de_roles) == 0: #si no existe el rol de admin, se crea
+                                permiso_de_admin = Permisos.objects.create(nombre="Permiso de administrador", descripcion="Permiso para administrador de sistema.")
+                                admin = RolSistema.objects.create(nombre="admin", descripcion="Administrador del sistema.")
+                                admin.permisos.add(permiso_de_admin)
+                                request.user.rolSistema.add(admin)
+                        else: #si existe el rol de admin, se le asigna directamente al usuario
+                                admin = RolSistema.objects.get(nombre="admin")
+                                request.user.rolSistema.add(admin)
+                return render(request, 'index.html')
+
+
+
+
 
