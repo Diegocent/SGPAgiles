cc8e24d8047f78dac99f45269470244e4cfe4ce0
marcelo <marce_molas@gmail.com>
2022-11-04 14:53:13 -0300

    a

--- iniciar_SGPAgiles.sh
+++ iniciar_SGPAgiles.sh
@@ -7,6 +7,7 @@ echo "Elija cómo se va a iniciar el proyecto:"
 echo "1 - Como lo dejé."
 echo "2 - Desde cero."
 echo "3 - Desde cero pero con datos pre-cargados."
+echo "4 - PRODUCCION."
 read -p 'Input: ' eleccion
 
 if [[ $eleccion -eq 1 ]]; then
@@ -17,6 +18,8 @@ elif [[ $eleccion -eq 2 ]]; then
 elif [[ $eleccion -eq 3 ]]; then
   docker compose down -v
   docker compose -f docker-compose-test.yml up --build
+elif [[ $eleccion -eq 4 ]]; then
+  docker compose -f docker-compose.yml -f docker-compose.prod.yml up --build
 else
   echo "no valido"
 fi
